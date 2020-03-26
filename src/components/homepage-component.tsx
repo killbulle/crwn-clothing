91309872bdf1d11f70220a61693883f70a47a004
killbulle <marco.godin@gmail.com>
2020-03-26 12:11:58 +0100

    init

---
+++ src/components/homepage-component.tsx
@@ -0,0 +1,29 @@
+import React, { FunctionComponent } from 'react';
+
+
+export const HomePage: FunctionComponent = () => (
+  <div className="homepage">
+    <div className="directory-menu" />
+    <div className="menu-item">
+      <div className="content">
+        <h1>HATS</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+    <div className="menu-item">
+      <div className="content">
+        <h1>JEAN</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+    <div className="menu-item">
+      <div className="content">
+        <h1>HATS</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  </div>
+);
+
+
+export default HomePage;
